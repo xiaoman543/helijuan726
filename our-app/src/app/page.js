572330a858499ead6fbc0924706ee6621420823d
xiaoman543 <shisc_0508@qq.com>
2025-05-08 00:36:51 +0800

    ‎07-async-01.html

---
+++ our-app/src/app/page.js
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+function Hello() {
+  return (
+      <div style={{ textAlign: 'center' }}>
+          <h1 className="text-Violet-300 bg-lime-200" style={{ fontSize: '2.5rem' }}>Hello World!</h1>
+          <p style={{ fontSize: '1.25rem' }}>Welcome to my website</p>
+      </div>
+  );
+}
+export default Hello;
